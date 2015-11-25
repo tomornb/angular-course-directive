355ea3d1dac3388b8279b9b9be4129942b2f0e6c
tomornb <admin@local.host>
2015-11-25 17:00:48 +0700

    -

--- 4_directive_transclusion/index.js
+++ 4_directive_transclusion/index.js
@@ -2,6 +2,8 @@ var app = angular.module("myApp", []);
 app.directive("container", function() {
     return {
         restrict: "E",
-        template: '<div class="panel">default message</div>'
+        transclude: true,
+        template: '<div class="panel" ng-transclude>' +
+        'default message</div>'
     };
 });
